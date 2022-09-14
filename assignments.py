25d2eec36bd8124af3e6d7c5408c062a0914e307
Coburn <coburncodes@gmail.com>
2022-09-14 14:24:10 -0700

    Add main and pset0 function

--- assignments.py
+++ assignments.py
@@ -1,20 +1,28 @@
 import time
 
-print("\nWelcome to MIT's Intro to Python Course!\n")
-time.sleep(1)
+def main():
+    print("\nWelcome to MIT's Intro to Python Course!\n")
+    time.sleep(1)
 
-print("Here, you can find my responses to each pset.\n")
-time.sleep(1)
+    print("Here, you can find my responses to each pset.\n")
+    time.sleep(1)
 
-while True:
-    try:
-        selection = int(input("Input the pset you'd like to see! "))
-    except (ValueError):
-        print("Please enter a number.")
-        continue
+    while True:
+        try:
+            selection = int(input("Input the pset you'd like to see! "))
+        except (ValueError):
+            print("Please enter a number.")
+            continue
 
-    if (selection > 5) or (selection < 0):
-        print("Invalid response :(\npsets start at 0 and end at 5.\nPlease input a number from 0-5.\n")
-    else:
-        print("Thanks! input =", selection)
-        break
+        if (selection > 5) or (selection < 0):
+            print("Invalid response :(\npsets start at 0 and end at 5.\nPlease input a number from 0-5.\n")
+        else:
+            if selection == 0:
+                pset0()
+            break
+
+def pset0():
+    print("Welcome to pset0!")
+
+
+main()
