e237f4c82ab9649264b69ddba06f617c94658b41
Coburn <coburncodes@gmail.com>
2022-09-26 17:27:59 -0700

    add p3

--- edx_psets.py
+++ edx_psets.py
@@ -20,6 +20,9 @@ def main():
             if selection == 2:
                 p2()
                 break
+            if selection == 3:
+                p3()
+                break
 
     
 
@@ -28,7 +31,7 @@ def p1():
     # Write a program that counts up the number of vowels contained 
     # in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
     # For example, if s = 'azcbobobegghakl', your program should print:
-    # Number of vowels: 5
+        # Number of vowels: 5
 
     s = "azcbobobegghakl"
     vowels = "aeiou"
@@ -47,7 +50,7 @@ def p2():
     # Write a program that prints the number of times the string 
     # 'bob' occurs in s. For example, if s = 'azcbobobegghakl', 
     # then your program should print:
-    # Number of times bob occurs is: 2
+        # Number of times bob occurs is: 2
 
     s = "azcbobobegghakl"
     key = "bob"
@@ -59,5 +62,40 @@ def p2():
         i += 1
 
     print("Number of times bob occurs is:", count)
+
+def p3():
+    # Assume s is a string of lower case characters.
+    # Write a program that prints the longest substring of 
+    # s in which the letters occur in alphabetical order. 
+    # For example, if s = 'azcbobobegghakl', then your program 
+    # should print:
+        # Longest substring in alphabetical order is: beggh
+    # In the case of ties, print the first substring. 
+    # For example, if s = 'abcbcd', then your program should print:
+        # Longest substring in alphabetical order is: abc
+
+    s = "azcbobobegghakl"
+    longest = []
+    count = 0
+    i = -1
+    j = 0
+
+    while i < len(s) - 1 and count < len(s):
+        count += 1
+        chain = [s[i + 1]]
+        while j < len(s) - 1:
+            i += 1
+            j += 1
+            if len(chain) > len(longest):
+                longest = chain
+            if s[i] <= s[j]:
+                chain.append(s[j])
+                continue
+            else:
+                chain = []
+                break
+
+    print("Longest substring in alphabetical order is:", "".join(longest))
+
 
 main()
