df3c823d548c7f9b1d9350fa324ee1e07d6d7322
Coburn <coburncodes@gmail.com>
2022-09-27 15:45:55 -0700

    add unit 2 problem 1

--- edx_psets.py
+++ edx_psets.py
@@ -1,32 +1,34 @@
 import time
 
 def main():
+
+    options = ["1.1", "1.2", "1.3", "2.1", "2.2", "2.3"]
+
     print("\nWelcome to the edX problems!\n")
     time.sleep(0.5)
 
     while True:
-        try:
-            selection = int(input("Input the problem you'd like to see: "))
-        except (ValueError):
-            print("Please enter a number.")
-            continue
+        selection = input("Input the problem you'd like to see: ")
 
-        if (selection > 6) or (selection < 1):
-            print("Invalid response :(\nproblems start at 1 and end at 10.\nPlease input a number from 1-10.\n")
+        if selection not in options:
+            print("Invalid response :(\nTry again\n")
         else:
-            if selection == 1:
-                p1()
+            if selection == "1.1":
+                u1p1()
                 break
-            if selection == 2:
-                p2()
+            if selection == "1.2":
+                u1p2()
                 break
-            if selection == 3:
-                p3()
+            if selection == "1.3":
+                u1p3()
+                break
+            if selection == "2.1":
+                u2p1()
                 break
 
     
 
-def p1():
+def u1p1():
     # Assume s is a string of lower case characters.
     # Write a program that counts up the number of vowels contained 
     # in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
@@ -45,7 +47,7 @@ def p1():
     print("Number of vowels:", count)
 
 
-def p2():
+def u1p2():
     # Assume s is a string of lower case characters.
     # Write a program that prints the number of times the string 
     # 'bob' occurs in s. For example, if s = 'azcbobobegghakl', 
@@ -63,7 +65,7 @@ def p2():
 
     print("Number of times bob occurs is:", count)
 
-def p3():
+def u1p3():
     # Assume s is a string of lower case characters.
     # Write a program that prints the longest substring of 
     # s in which the letters occur in alphabetical order. 
@@ -96,6 +98,28 @@ def p3():
                 break
 
     print("Longest substring in alphabetical order is:", "".join(longest))
+
+
+def u2p1():
+    # Write a program to calculate the credit card balance 
+    # after one year if a person only pays the minimum monthly 
+    # payment required by the credit card company each month.
+
+    # Variables
+    balance = 484
+    annual_interest_rate = 0.20
+    monthly_payment_rate = 0.04
+
+    # Calculated variables
+    monthly_interest_rate = annual_interest_rate / 12.0
+
+    for i in range(12):
+        balance = balance * (1 + monthly_interest_rate)
+        minimum_monthly_payment = monthly_payment_rate * balance
+        balance = balance - minimum_monthly_payment
+    
+    print("Remaining balance:", round(balance, 2))
+
 
 
 main()
